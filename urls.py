bb69cbf96375145e7e242ea3fb7b9cb3f6343f3f
Karim Nagib <karimn@bu.edu>
2010-05-12 21:40:39 -0400

    Adding view

--- urls.py
+++ urls.py
@@ -1,6 +1,5 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
@@ -12,7 +11,8 @@ urlpatterns = patterns('',
     # to INSTALLED_APPS to enable admin documentation:
     # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
+    (r'^population/$', 'colonialismdb.population.views.index'),
+
     (r'^admin/', include(admin.site.urls)),
 )
 
