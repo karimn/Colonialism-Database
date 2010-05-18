4baa35524d3a463752d1a438cc224271ba9246ee
Karim Nagib <karimn@bu.edu>
2010-05-17 22:33:03 -0400

    Adding data migration code

--- population/models.py
+++ population/models.py
@@ -5,14 +5,20 @@ from django.db import models
 class Location(models.Model):
   name = models.CharField("Name", max_length = 50)
   in_location = models.ForeignKey('self', null = True, blank = True, default = None, verbose_name = "In")
-# class Ethnicity(models.Model): #  name = models.CharField(max_length = 50, unique = True)
+
+  def __unicode__(self): 
+    if self.in_location:
+      return self.name + ", " + unicode(self.in_location)
+    else:
+      return self.name 
+
 
 class MainDataEntry(models.Model):
   RELIGION_CHOICES = ((0, 'None'), (1, 'Christian'), (2, 'Muslim'), (3, 'Jew'))
-  RACE_CHOICES = ((0, 'White'), (1, 'Black'))
+  RACE_CHOICES = (('Whites', 'White'), ('Negroes', 'Black'))
   ETHNICITY_CHOICES = ((0, 'Indian'), (1, 'Russian')) 
   ETHNIC_ORIGIN_CHOICES = ((0, 'Foreign'), (1, 'Indigenous')) 
-  UNIT_CHOICES = ((1, 'Hundreds'), (2, 'Thousands'), (3, 'Millions'))
+  UNIT_CHOICES = (('hundreds', 'Hundreds'), ('thousands', 'Thousands'), ('millions', 'Millions'))
   POP_COND_CHOICES = ((0, 'None'), (1, 'Dead'), (2, 'Educated'), (3, 'Enslaved'), (4, 'Illiterate'), (5, 'Impaired'))
 
   class Meta:
@@ -26,22 +32,25 @@ class MainDataEntry(models.Model):
   original_location_name = models.CharField("Original Location Name", max_length = 50, null = True, blank = True, help_text = "Original name used in the source if different from the English name")
   alternate_location_name = models.CharField("Alternate Location Name", max_length = 50, null = True, blank = True)
   religion = models.IntegerField(choices = RELIGION_CHOICES, null = True, blank = True)
-  race = models.IntegerField(choices = RACE_CHOICES, null = True, blank = True)
+  race = models.CharField(max_length = 15, choices = RACE_CHOICES, null = True, blank = True)
   ethnicity = models.IntegerField(choices = ETHNICITY_CHOICES, null = True, blank = True)
   ethnic_origin = models.IntegerField("Ethnic Origin", choices = ETHNIC_ORIGIN_CHOICES, null = True, blank = True)
   age_start = models.CharField("Start Age", max_length = 50, default = 'Unknown', help_text = "Years old at beginning of age grouping")
   age_end = models.CharField("End Age", max_length = 50, default = 'Unknown', help_text = "Years old at end of age grouping")
   remarks = models.TextField(null = True, blank = True)
   # TODO Link field
-  individuals_population_value = models.IntegerField("Individuals", null = True, blank = True)
-  families_population_value = models.IntegerField("Families", null = True, blank = True)
-  male_population_value = models.IntegerField("Male", null = True, blank = True)
-  female_population_value = models.IntegerField("Female", null = True, blank = True)
-  value_unit = models.IntegerField("Units", choices = UNIT_CHOICES, null = True, blank = True)
+  individuals_population_value = models.DecimalField("Individuals", max_digits = 10, decimal_places = 2, null = True, blank = True)
+  families_population_value = models.DecimalField("Families", max_digits = 10, decimal_places = 2, null = True, blank = True)
+  male_population_value = models.DecimalField("Male", max_digits = 10, decimal_places = 2, null = True, blank = True)
+  female_population_value = models.DecimalField("Female", max_digits = 10, decimal_places = 2, null = True, blank = True)
+  value_unit = models.CharField("Units", max_length = 15, choices = UNIT_CHOICES, null = True, blank = True)
   is_total = models.BooleanField("Is Total", default = False)
   population_condition = models.IntegerField("Population Condition", choices = POP_COND_CHOICES, default = 0)
   polity = models.CharField(max_length = 100, null = True, blank = True)
   iso = models.CharField("ISO", max_length = 100, null = True, blank = True) 
   wb = models.CharField("WB", max_length = 100, null = True, blank = True)
+
+  def __unicode(self) :
+    return "%s (%s - %s)" % (self.location, self.begin_date, self.end_date)
 
   
