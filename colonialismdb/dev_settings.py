c295787c51814902869b722a4ba607acd29cfc7b
Karim Nagib <karimn@bu.edu>
2010-07-14 07:06:33 -0400

    Preparing settings for prod

---
+++ colonialismdb/dev_settings.py
@@ -0,0 +1,6 @@
+from settings import *
+
+DEBUG = True
+TEMPLATE_DEBUG = DEBUG
+
+
---
+++ colonialismdb/prod_settings.py
@@ -0,0 +1,18 @@
+from settings import *
+
+DEBUG = False 
+TEMPLATE_DEBUG = DEBUG
+
+ADMINS = (
+  ('Karim Nagib', 'karimn@bu.edu'),
+    # ('Your Name', 'your_email@domain.com'),
+)
+
+MANAGERS = ADMINS
+
+# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
+# trailing slash.
+# Examples: "http://foo.com/media/", "/media/".
+ADMIN_MEDIA_PREFIX = '/admin_media/'
+
+
