0f0346e3c32dc5020a82c60d4431021b4e97593c
Karim Nagib <karimn@bu.edu>
2010-07-29 10:42:14 -0400

    Changing some errors to warnings for now

--- linkpopdata2table.py
+++ linkpopdata2table.py
@@ -7,6 +7,7 @@ from colonialismdb.population.models import MainDataEntry
 from colonialismdb.sources.models import Table
 
 num_err_rows = 0
+num_wrn_rows = 0
 
 for de in MainDataEntry.objects.all():
   cid_matches = re.match(r'([^-]+)-([^\.]+)\.(.+)', de.combined_id)
@@ -23,12 +24,15 @@ for de in MainDataEntry.objects.all():
   table = Table.objects.get(old_id = table_id)
 
   if table.old_source_id != de.source_id:
-    sys.stderr.write('Returned table %s did not match old source id for %s\n' % (table, de))
-    num_err_rows += 1
-    continue
+    sys.stderr.write('Waring: returned table with source id %i did not match old source id for %i\n' % (table.old_source_id, de.source_id))
+    num_wrn_rows += 1
+
+  if table.nr != nr:
+    sys.stderr.write('Warning: returned table nr %s did not match data entry nr %s\n' % (table.nr, nr))
+    num_wrn_rows += 1
 
   de.source_table = table
   de.save()
 
-sys.stdout.write('Linking completed.  %i error(s) encountered\n' % num_err_rows)
+sys.stdout.write('Linking completed.  %i error(s) and %i warning(s) encountered\n' % (num_err_rows, num_wrn_rows))
 
