e57da2556fe8a8d0543025dd9725b957032424c1
Karim Nagib <karimn@bu.edu>
2010-08-25 08:24:13 -0400

    really adding infrastruct models/admin

---
+++ colonialismdb/infrastructure/admin.py
@@ -0,0 +1,43 @@
+from colonialismdb.infrastructure.models import MainDataEntry
+from colonialismdb.sources.models import Table, SpatialAreaUnit, Currency 
+from colonialismdb.common.admin import BaseSubmitAdmin, BaseMergeableCategoryAdmin, BaseMainDataEntryAdmin
+from django.contrib import admin
+
+class MainDataEntryAdmin(BaseMainDataEntryAdmin) :
+  fieldsets = [
+      (None,
+        {'fields' : ['active', 'submitted_by']}),
+
+      ('Location Information', 
+        {'fields' : ['location', 'original_location_name', 'alternate_location_name', 'spatial_area', 'spatial_area_unit']}),
+      
+      ('Date Range', 
+        {'fields' : ['begin_date', 'end_date', 'circa']}),
+
+      ('Currency',
+        {'fields' : ['currency', 'currency_exchange_rate']}),
+
+      ('Railroad',
+        {'fields' : ['railroad_length', 'railroad_freight', 'railroad_revenue', 'railroad_expenditure']}),
+
+      ('Road',
+        {'fields' : ['road_length', ]}),
+
+      {'Telegraph',
+        {'fields' : ['telegraph_length', ]}),
+
+      ('Source Information', 
+        {'fields' : ['source', 'page_num', 'primary_source', 'polity', 'iso', 'wb']}),
+
+      ('Other Information', 
+        {'fields' : ['remarks', ], 'classes' : ['collapse', ]}),
+  ]
+
+  #radio_fields = { 'value_precision' : admin.HORIZONTAL, 'individ_fam' : admin.HORIZONTAL, 'population_gender' : admin.HORIZONTAL, 'value_unit' : admin.HORIZONTAL, }
+
+  list_display = ('location', 'begin_date', 'end_date', 'active', 'submitted_by')
+  ordering = ['begin_date']
+
+  activate_perm = 'infrastructure.activate_maindataentry'
+
+admin.site.register(MainDataEntry, MainDataEntryAdmin)
---
+++ colonialismdb/infrastructure/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from colonialismdb.common.models import Location, BaseDataEntry, Category, SpatialAreaUnit, Currency
+from colonialismdb.sources.models import BaseSourceObject 
+    
+class MainDataEntry(BaseDataEntry):
+  class Meta(BaseDataEntry.Meta):
+    verbose_name = "infrastructure data entry"
+    verbose_name_plural = "infrastructure data entries"
+    permissions = ( ('activate_maindataentry', 'Can activate submitted data entry'), )
+
+  # Should these be moved to location?
+  spatial_area = models.IntegerField(null = True, blank = True)
+  spatial_area_unit = models.ForeignKey(SpatialAreaUnit, null = True, blank = True)
+
+  currency = models.ForeignKey(Currency, null = True, blank = True)
+  currency_exchange_rate = models.IntegerField(null = True, blank = True)
+
+  railroad_length = models.IntegerField(null = True, blank = True)
+  railroad_freight = models.BooleanField(default = False)
+  railroad_revenue = models.IntegerField(null = True, blank = True)
+  railroad_expenditure = models.IntegerField(null = True, blank = True)
+
+  road_length = models.IntegerField(null = True, blank = True)
+  telegraph_length = models.IntegerField(null = True, blank = True)
+
+  def activate(self):
+    super(MainDataEntry, self).activate()
