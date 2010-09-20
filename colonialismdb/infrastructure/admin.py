76680b46f07c9831f26f8b0c46246ebea3445e93
Karim Nagib <karimn@bu.edu>
2010-09-20 12:10:05 -0400

    Adding missing fields in infrastructure admin

--- colonialismdb/infrastructure/admin.py
+++ colonialismdb/infrastructure/admin.py
@@ -19,13 +19,22 @@ class MainDataEntryAdmin(BaseMainDataEntryAdmin) :
         {'fields' : ['currency', 'currency_exchange_rate']}),
 
       ('Railroad',
-        {'fields' : ['railroad_length', 'railroad_freight', 'railroad_revenue', 'railroad_expenditure']}),
+        {'fields' : ['railroad_revenue', 'railroad_expenditure', 'railroad_length', 'railroad_length_unit', 'railroad_num_passengers', 'railroad_passenger_km', 'railroad_freight', 'railroad_freight_unit', ]}),
 
       ('Road',
-        {'fields' : ['road_length', ]}),
+        {'fields' : ['road_revenue', 'road_expenditure', 'road_length', 'road_length_unit', 'num_motor_vehicles', 'motor_vehicles_type', ]}),
 
-      ('Telegraph',
-        {'fields' : ['telegraph_length', ]}),
+      ('Telephones/Telegraph',
+        {'fields' : ['num_telephones', 'telegraph_length', 'telegraph_length_unit', 'telegraph_num_stations', 'telegraph_num_sent', ]}),
+
+      ('Postal',
+        {'fields' : ['postal_revenue', 'postal_expenditure', 'postal_num_stations', 'postal_num_items', 'postal_items_type', 'postal_num_boxes', 'postal_num_staff',  ]}),
+
+      ('Merchant Ships',
+        {'fields' : ['merchant_ships_num', 'merchant_ships_type', 'merchant_ships_cargo', 'merchant_ships_cargo_unit', ]}),
+
+      ('Aviation',
+        {'fields' : ['air_cargo', 'air_cargo_unit', 'air_passenger_km', ]}),
 
       ('Source Information', 
         {'fields' : ['source', 'page_num', 'primary_source', 'polity', 'iso', 'wb']}),
