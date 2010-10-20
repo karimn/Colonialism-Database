7392d6b635d8da58863c78cdc76aebe6c2640e78
Karim Nagib <karimn@bu.edu>
2010-10-20 10:44:40 -0400

    Cleanup loc script

---
+++ cleanloc.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+from django.db import transaction
+from django.contrib.auth.models import User
+
+from colonialismdb.common import PoliticalUnit, Location
+
+mig_user = User.objects.get(username = 'karim')
+
+def print_loc(query):
+  for l in query:
+    print("'%s', '%s', '%s', %s, %s", l.name, l.geographically_in, l.politically_in, l.geo_features, l.unit_types.count())
+
+@transaction.commit_manually
+def cleanup_turkey():
+  pass
+
+if __name__ == "__main__":
+  pass
+
