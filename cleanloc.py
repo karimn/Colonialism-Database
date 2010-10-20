b41dc699313cc5079452ad6bf1d8d731446d54aa
Karim Nagib <karimn@bu.edu>
2010-10-20 11:29:54 -0400

    turkey code

--- cleanloc.py
+++ cleanloc.py
@@ -4,6 +4,7 @@ from django.db import transaction
 from django.contrib.auth.models import User
 
 from colonialismdb.common.models import PoliticalUnit, Location
+from colonialismdb.common.admin import merge
 
 mig_user = User.objects.get(username = 'karim')
 
@@ -13,7 +14,9 @@ def print_loc(query):
 
 @transaction.commit_manually
 def cleanup_turkey():
-  pass
+  merge_into = Location.objects.filter(name__iexact = "Turkey").filter(geographically_in = None).filter(politically_in = None)[0]
+  merge(merge_into, Location.objects.filter(name__iexact = "Turkey").filter(geographically_in = None).filter(politically_in = None)[1:])
+  transaction.commit()
 
 if __name__ == "__main__":
   pass
