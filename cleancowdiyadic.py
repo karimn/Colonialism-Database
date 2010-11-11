901217c4067832dbd9b40cafedad82f769e05391
Karim Nagib <karimn@bu.edu>
2010-11-11 14:51:02 -0500

    Remove missing data

---
+++ cleancowdiyadic.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from django.db.models import Q
+
+from colonialismdb.economics.models import BilateralTradeDataEntry
+
+if __name__ == "__main__":
+  for bidata in BilateralTradeDataEntry.objects.filter(Q(exports = -9) | Q(imports = -9), Q(source = 3393)):
+    if bidata.imports == -9:
+      if bidata.exports == -9:
+        bidata.delete()
+      else:
+        bidata.exports = None
+        bidata.save()
+    else:
+      bidata.imports = None
+      bidata.save()
+
