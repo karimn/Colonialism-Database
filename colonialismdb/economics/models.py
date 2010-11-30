0f721258a32563174c8cd990f30fea1e51907f42
Karim Nagib <karimn@bu.edu>
2010-11-29 22:42:02 -0500

    Migrating W-H non-UK trade data

--- colonialismdb/economics/models.py
+++ colonialismdb/economics/models.py
@@ -14,6 +14,12 @@ class BaseTradeDataEntry(common.models.BaseDataEntry):
 
   currency = models.ForeignKey(common.models.Currency, null = True, blank = True, related_name = '%(app_label)s_%(class)s_related')
 
+  def activate(self):
+    super(BaseTradeDataEntry, self).activate()
+
+    if self.currency and not self.currency.active:
+      self.currency.activate()
+
 class AggregateTradeDataEntry(BaseTradeDataEntry):
   class Meta(BaseTradeDataEntry.Meta):
     verbose_name = "aggregate trade data entry"
@@ -28,17 +34,27 @@ class BilateralTradeDataEntry(BaseTradeDataEntry):
 
   trade_partner = models.ForeignKey(common.models.PoliticalUnit, related_name = '%(app_label)s_%(class)s_trade_partner_related')
 
-  imports_exchange_rate = models.DecimalField(max_digits = 13, decimal_places = 10, null = True, blank = True)
-  exports_exchange_rate = models.DecimalField(max_digits = 13, decimal_places = 10, null = True, blank = True)
-  gfd_exchange_rate = models.DecimalField(max_digits = 13, decimal_places = 10, null = True, blank = True)
-  exchange_rate_scalar = models.DecimalField(max_digits = 6, decimal_places = 4, null = True, blank = True, help_text = "GFD Rate/Imputed Rate")
+  imports_exchange_rate = models.DecimalField(max_digits = 20, decimal_places = 10, null = True, blank = True)
+  exports_exchange_rate = models.DecimalField(max_digits = 20, decimal_places = 10, null = True, blank = True)
+  gfd_exchange_rate = models.DecimalField(max_digits = 20, decimal_places = 10, null = True, blank = True)
+  exchange_rate_scalar = models.DecimalField(max_digits = 10, decimal_places = 4, null = True, blank = True, help_text = "GFD Rate/Imputed Rate")
 
   imports_weight = models.DecimalField(max_digits = 18, decimal_places = 10, null = True, blank = True)
   exports_weight = models.DecimalField(max_digits = 18, decimal_places = 10, null = True, blank = True)
   imports_weight_unit = models.ForeignKey(common.models.WeightUnit, blank = True, null = True, related_name = '%(app_label)s_%(class)s_imports_related')
   exports_weight_unit = models.ForeignKey(common.models.WeightUnit, blank = True, null = True, related_name = '%(app_label)s_%(class)s_exports_related')
 
-  ppi = models.DecimalField("producer price index", max_digits = 12, decimal_places = 10, null = True, blank = True)
+  def activate(self):
+    super(BilateralTradeDataEntry, self).activate()
+
+    if self.imports_weight_unit and not self.imports_weight_unit.active:
+      self.imports_weight_unit.activate()
+
+    if self.exports_weight_unit and not self.exports_weight_unit.active:
+      self.exports_weight_unit.activate()
+
+    if self.trade_partner and not self.trade_partner.active:
+      self.trade_partner.activate()
 
   def __unicode__(self):
     return "%s + %s (%s - %s)" % (unicode(self.location), unicode(self.trade_partner), self.begin_date, self.end_date)
