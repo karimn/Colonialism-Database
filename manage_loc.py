da0a8cb9442f2fef7ee6bd5b7f9f1700e81bd56a
Karim Nagib <karimn@bu.edu>
2010-12-14 13:10:17 -0500

    Print only

--- manage_loc.py
+++ manage_loc.py
@@ -17,17 +17,17 @@ if __name__ == "__main__":
 
     for l in Location.get_toplevel().filter(name__iexact = loc_name):
       print("%s (politically in %s)" % (l.name, l.politically_in))
+  else:
+    loc_dict = dict()
+    for l in Location.get_toplevel():
+      l_name = l.name.lower()
+      if loc_dict.has_key(l_name):
+        loc_dict[l_name].append(l)
+      else:
+        loc_dict[l_name] = [l,]
 
-  loc_dict = dict()
-  for l in Location.get_toplevel():
-    l_name = l.name.lower()
-    if loc_dict.has_key(l_name):
-      loc_dict[l_name].append(l)
-    else:
-      loc_dict[l_name] = [l,]
-
-  for l_name, l_list in loc_dict.iteritems():
-    if len(l_list) > 1:
-      print("%s : %i" % (l_name, len(l_list)))
+    for l_name, l_list in loc_dict.iteritems():
+      if len(l_list) > 1:
+        print("%s : %i" % (l_name, len(l_list)))
 
 
