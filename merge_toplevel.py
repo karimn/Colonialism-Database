670d06df01ed2cf3ea0ba7f32d7acfb2e1b46bbb
Karim Nagib <karimn@bu.edu>
2010-12-14 13:06:26 -0500

    merge_toplevel cleanup script

---
+++ merge_toplevel.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+
+import sys
+
+from django.db import transaction
+from django.db.models import Q
+from django.contrib.auth.models import User
+
+from colonialismdb.common.models import PoliticalUnit, Location
+from colonialismdb.common.admin import merge
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    loc_name = sys.argv[1]
+    merge_into = Location.get_toplevel().filter(name__iexact = loc_name)[0]
+
+    for l in Location.get_toplevel().filter(name__iexact = loc_name)[1:]:
+      l.merge_into(merge_into)
+      l.save()
+
+    for l in Location.get_toplevel().filter(name__iexact = loc_name)[1:]:
+      l.delete()
+
+    merge_into.save()
+
+    if len(sys.argv) > 3:
+      if sys.argv[2] == "pk":
+        put_in_loc = Location.objects.get(pk = sys.argv[3])
+      else:
+        put_in_loc = Location.toplevel().get(name__iexact = sys.argv[3])
+
+
