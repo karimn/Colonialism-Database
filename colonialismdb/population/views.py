290fc8bb9da3fd0a9b84f6b45daaf0db60832b4a
Sriram Murali <sriram@bu.edu>
2011-02-22 15:14:54 +0800

    Completed search functions on all categories.

--- colonialismdb/population/views.py
+++ colonialismdb/population/views.py
@@ -4,63 +4,73 @@ from django.db.models import Q
 from django.contrib import auth
 from django.http import *
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.utils.encoding import smart_str, smart_unicode
 
-def	popsearch(request):
-	startdate = request.GET.get('startdate','')
-	enddate = request.GET.get('enddate','')
-	page = request.GET.get('page','')
-	search = request.GET.get('search','')
-	location = request.GET.get('location','')
-	locations2 = []
-
+def popsearch(request):
+	locations_list = []
 	for x in MainDataEntry.objects.select_related():
-		if not x.location.location.name in locations2:
-			locations2.append(str(x.location.location.name))
-	
-	locations = str(locations2)
-
-
-	if search == "Search date values":
-	#	if not startdate or startdate == "":
-	#		startdate = "1870-01-01"
-	#	if not enddate or enddate == "":
-	#		enddate = "2006-12-31"
-		if startdate and enddate:
-			qset = (Q(begin_date__range=(startdate,enddate)) | Q(end_date__range=(startdate,enddate)))
-			disp = MainDataEntry.objects.filter(qset).select_related().order_by('id')
-			paginator = Paginator(disp,1)
-			try:
-				page = int(request.GET.get('page',1))
-			except ValueError:
-				page = 1
-			try:
-				disp = paginator.page(page)
-			except (EmptyPage, InvalidPage):
-				disp = paginator.page(paginator.num_pages)
+		clocation = smart_unicode(x.location.location.name,encoding='utf-8',errors='ignore')
+		if not  clocation in locations_list:
+			#locations_list.append(smart_str(x.location.location.name))
+			locations_list.append(clocation)
+	if request.GET.get('datesearch'):
+		datesearch = request.GET.get('datesearch')
+		startdate = request.GET.get('startdate')
+		enddate = request.GET.get('enddate')
+		qset = (Q(begin_date__range=(startdate,enddate)) | Q(end_date__range=(startdate,enddate)))
+		dateresults = MainDataEntry.objects.filter(qset).select_related().order_by('id')
+		paginator = Paginator(dateresults,1)
+		try:
+			page = int(request.GET.get('page',1))
+		except ValueError:
+			page = 1
+		try:
+			dateresults = paginator.page(page)
+		except (EmptyPage, InvalidPage):
+			dateresults = paginator.page(paginator.num_pages)
 	else:
-		disp = []
-	"""
-	if search == "Search location values":
-		if location:
-			qset = (Q(begin_date__range=(startdate,enddate)) | Q(end_date__range=(startdate,enddate)))
-			disp = AggregateTradeDataEntry.objects.filter(qset).values().order_by('id')
-			paginator = Paginator(disp,1)
+		datesearch = ""
+		startdate = ""
+		enddate = ""
+		dateresults = []
+	if request.GET.get('locationsearch'):
+		locationsearch = request.GET.get('locationsearch')
+		searchlocations = request.GET.get("locations")
+		if searchlocations:
+			loclist = searchlocations.split(", ")
+			qs = ""
+			locresults = []
+			for x in loclist:
+				locresults += MainDataEntry.objects.filter(Q(location__name="%s" % x)).select_related().order_by('id')
+			paginator = Paginator(locresults,1)
 			try:
-				page = int(request.GET.get('page',1))
+				page = request.GET.get('page','1')
 			except ValueError:
-				page = 1
+				page = '1'
 			try:
-				disp = paginator.page(page)
+				locresults = paginator.page(page)
 			except (EmptyPage, InvalidPage):
-				disp = paginator.page(paginator.num_pages)
+				locresults = paginator.page(paginator.num_pages)
 	else:
-		disp = []
-	"""
-
-	"""Source search
-	for x in AggregateTradeDataEntry.objects.select_related():
-		l.append(str(x.source.source.source.source))
-	"""
-
-
-	return render_to_response("population.html",{"results":disp,"sdate":startdate,"edate":enddate,"query":enddate,"location":location, "search":search, "locations":locations})
+		searchlocations = ""
+		locationsearch = ""
+		locresults = []
+	if request.GET.get('sourcesearch'):
+		sourcesearch = request.GET.get('sourcesearch')
+		sourceinput = request.GET.get('sourceinput')
+		sourceresults = MainDataEntry.objects.select_related().filter(Q(source__name__icontains=sourceinput)).order_by('id')
+		paginator = Paginator(sourceresults,1)
+		try:
+			page = request.GET.get('page','1')
+		except ValueError:
+			page = '1'
+		try:
+			sourceresults = paginator.page(page)
+		except (EmptyPage, InvalidPage):
+			sourceresults = paginator.page(paginator.num_pages)
+	else:
+		sourcesearch = ""
+		sourceinput = ""
+		sourceresults = []
+		
+	return render_to_response("population.html",{"locations_list":locations_list,"searchlocations":searchlocations,"locresults":locresults,"locationsearch":locationsearch,"datesearch":datesearch,"startdate":startdate,"enddate":enddate,"dateresults":dateresults,"sourcesearch":sourcesearch,"sourceinput":sourceinput,"sourceresults":sourceresults})
